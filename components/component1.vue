b84a63474c2433f0ab863f79a182fc0455842a36
Marco Gattelli <marco.gattelli@intelygenz.com>
2018-05-25 13:32:01 +0200

    - committing a new page (login) and its component. - committing some new components in order to explain how the vue-test-utils works. - adding some dependencies in order to run the tests.

---
+++ components/component1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p id="pcomponent">paragrafo desde el componente 1</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'component1'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/marco.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input id="val1" type="text" v-model="val1">
+    <p id="val1p">{{val1}}</p>
+    <component1></component1>
+  </div>
+</template>
+
+<script>
+import component1 from './component1'
+export default {
+  name: 'marco',
+  components: {
+    component1
+  },
+  data() {
+    return {
+      val1: 'value1'
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
